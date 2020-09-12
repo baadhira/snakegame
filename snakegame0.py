b7cc18ed2837940ebfde066594d9cdf285a50f1c
BADIRA P P <badirakunhabdulla@gmail.com>
2020-09-12 12:27:55 +0530

    First commit

---
+++ snakegame0.py
@@ -0,0 +1,20 @@
+import pygame,sys,os
+from pygame.locals import*
+red = [255,0,0]
+
+#initialise pygame
+pygame.init()
+
+#setup window
+window = pygame.display.set_mode((1000,600))
+pygame.display.set_caption("slither.eat-The Snake Game")
+
+#setup drawing surface
+screen=pygame.display.get_surface()
+screen.fill(red)
+pygame.display.set_caption("snake")
+pygame.display.flip()
+while True:
+      print("slither.eat-The Snake Game")
+      pass
+
